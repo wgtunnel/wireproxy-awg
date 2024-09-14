fe2920a47dbada6773e385b8a646d860e234cf43
Artem Russkikh <rusartx@gmail.com>
2024-09-14 23:32:53 +0500

    Sync fork (#3)
    * Update README (#137)
    
    * Fix broken sandboxing resulting in SIGABRT (#136)
    
    * Fix HTTP proxy authentication to support both preemptive and challenge-response auth (#134)
    
    * Bump version
    
    ---------
    
    Co-authored-by: Niko <niko@niko.lgbt>
    Co-authored-by: Luiz Henrique Gomes Palácio <lhpalacio@outlook.com>

--- cmd/wireproxy/main.go
+++ cmd/wireproxy/main.go
@@ -28,7 +28,7 @@ var default_config_paths = []string {
     os.Getenv("HOME")+"/.config/wireproxy.conf",
 }
 
-var version = "1.0.8-dev"
+var version = "1.0.11-dev"
 
 func panicIfError(err error) {
 	if err != nil {
